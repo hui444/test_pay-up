07280d461cc1220404e969aa1f150cabaad6fd3b
hui444 <huihui.uni@yahoo.com>
2024-12-16 00:20:15 +0800

    Deploy Version 1.1.2+1
    Changelog:
    - Fix recent currency not updating bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "390bafb112e3469f0a54ca3395ffda48",
+const RESOURCES = {"flutter_bootstrap.js": "0b496039ee95e83132e22250676b02f8",
 "version.json": "f64755f3a509b8cc35f76e614f623387",
 "index.html": "e868903bf55bfc2d5ca981b63bfe0d84",
 "/": "e868903bf55bfc2d5ca981b63bfe0d84",
-"main.dart.js": "ff3bd0a3a84dbdef6eab3651ea081fd6",
+"main.dart.js": "bf48a73bae88a1c82804e6ddafdf8b95",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "c4f2f166f46943a5101faefb3326c47c",
 "icons/Icon-192.png": "07116851ebc9ccbfdb119f70f1623feb",
